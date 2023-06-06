5a565b82fa724462adec737a8138028a36f3da36
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-06 17:14:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+import logging
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+logging.basicConfig(format='%(asctime)s - %(message)s',level=logging.DEBUG)
 
 capabilities = {
     "browserName": "firefox",
     "version": "88.0",
     "platform": "LINUX"
 }
+
 driver = webdriver.Remote(
     command_executor='http://localhost:4444/wd/hub',
     desired_capabilities=capabilities
 )
+
 try:
-    print('Session ID is: %s' % driver.session_id)
-    print('Opening the page...')
+    logging.info('Session ID is: %s' % driver.session_id)
+    #print('Session ID is: %s' % driver.session_id)
+    logging.info('Opening the page...')
+    #print('Opening the page...')
     driver.get("http://www.python.org")
     assert "Python" in driver.title
     elem = driver.find_element(By.NAME, "q")
@@ -21,7 +28,8 @@ try:
     elem.send_keys("pycon")
     elem.send_keys(Keys.RETURN)
     assert "No results found." not in driver.page_source
-    print('Taking screenshot...')
+    logging.info('Taking screenshot...')
+    #print('Taking screenshot...')
     driver.get_screenshot_as_file(driver.session_id + '.png')
 finally:
     driver.quit()
