63b1165207042d65a0f417826dd758c43aaa3982
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-06 13:03:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -10,16 +10,16 @@ driver = webdriver.Remote(
     desired_capabilities=capabilities
 )
 try:
-print 'Session ID is: %s' % driver.session_id
-print 'Opening the page...'
- driver.get("http://www.python.org")
- assert "Python" in driver.title
- elem = driver.find_element_by_name("q")
- elem.clear()
- elem.send_keys("pycon")
- elem.send_keys(Keys.RETURN)
- assert "No results found." not in driver.page_source
-print 'Taking screenshot...'
-driver.get_screenshot_as_file(driver.session_id + '.png')
+    print('Session ID is: %s' % driver.session_id)
+    print('Opening the page...')
+    driver.get("http://www.python.org")
+    assert "Python" in driver.title
+    elem = driver.find_element_by_name("q")
+    elem.clear()
+    elem.send_keys("pycon")
+    elem.send_keys(Keys.RETURN)
+    assert "No results found." not in driver.page_source
+    print('Taking screenshot...')
+    driver.get_screenshot_as_file(driver.session_id + '.png')
 finally:
     driver.quit()
