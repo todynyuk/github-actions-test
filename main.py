dfa5403b376a5785079eb51e35e1ac510760a742
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-06 12:48:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,29 @@
 from selenium import webdriver
-
-HOST = "localhost"
-
-def test_firefox():
-    capabilities = {
-        "browserName": "firefox",
-        "version": "108.0",
-        "enableVNC": False,
-        "enableVideo": False
-    }
-    firefox = webdriver.Remote(command_executor='http://{}:4444/wd/hub'.format(HOST),
-                               desired_capabilities=capabilities)
-    firefox.get('https://www.google.com')
-    print('firefox', firefox.title)
-    title=firefox.title
-    assert title=="Google", "Title not are Google"
-    firefox.quit()
-
-def test_chrome():
-    capabilities = {
-        "browserName": "chrome",
-        "version": "108.0_VNC",
-        "enableVNC": True,
-        "enableVideo": False
-    }
-    chrome = webdriver.Remote(command_executor='http://{}:4444/wd/hub'.format(HOST),
-                              desired_capabilities=capabilities)
-    chrome.get('https://www.google.com')
-    print('chrome', chrome.title)
-    assert title=="Google", "Title not are Google"
-    chrome.quit()
-
-
-if __name__ == "__main__":
-    test_firefox()
-    test_chrome()
+from selenium.webdriver.common.keys import Keys
+ 
+capabilities = {
+    "browserName": "chrome",
+    "version": "90.0",
+    "platform": "LINUX"
+}
+ 
+driver = webdriver.Remote(
+    command_executor='http://localhost:4444/wd/hub',
+    desired_capabilities=capabilities
+)
+ 
+try:
+print 'Session ID is: %s' % driver.session_id
+print 'Opening the page...'
+ driver.get("http://www.python.org")
+ 
+ assert "Python" in driver.title
+ elem = driver.find_element_by_name("q")
+ elem.clear()
+ elem.send_keys("pycon")
+ elem.send_keys(Keys.RETURN)
+ assert "No results found." not in driver.page_source
+print 'Taking screenshot...'
+driver.get_screenshot_as_file(driver.session_id + '.png')
+finally:
+    driver.quit()
