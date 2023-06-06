397a85f9bfa130ba232fdeb3a9aa74a248e154be
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-06 14:04:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
 capabilities = {
     "browserName": "firefox",
     "version": "88.0",
@@ -14,7 +16,7 @@ try:
     print('Opening the page...')
     driver.get("http://www.python.org")
     assert "Python" in driver.title
-    elem = driver.find_element_by_name("q")
+    elem = driver.find_element(By.NAME, "q")
     elem.clear()
     elem.send_keys("pycon")
     elem.send_keys(Keys.RETURN)
