ba0abd4e48d0f022ea5ba7d3383477c98f754837
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-09 13:40:37 +0300

    Update TestOptions.py

--- src/TestCases/TestOptions.py
+++ src/TestCases/TestOptions.py
@@ -10,15 +10,22 @@ def test_options():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     driver.get("https://www.whatismybrowser.com/")
     driver.maximize_window()
     time.sleep(5)
     logging.info("browser opened")
     logging.info(os.getcwd())
-    if os.path.exists(os.path.join(os.getcwd(),'output')):
-        shutil.rmtree(os.path.join(os.getcwd(),'output'))
+    if os.path.exists(os.path.join(os.getcwd(), 'output')):
+        shutil.rmtree(os.path.join(os.getcwd(), 'output'))
 
     os.mkdir('output')
+    f_handler = logging.FileHandler('output/report.log')
+
+
+    formatter = logging.Formatter('%(asctime)s  - %(levelname)s - %(message)s')
+
+    f_handler.setFormatter(formatter)
+    logger.addHandler(f_handler)
     driver.save_screenshot('output/screen.png')
     driver.close()
