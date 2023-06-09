ac72b7cdde7f4695ec840063977ed6f11e0032f9
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-09 10:54:41 +0300

    Update TestOptions.py

--- src/TestCases/TestOptions.py
+++ src/TestCases/TestOptions.py
@@ -9,7 +9,7 @@ def test_options():
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),chrome_options=options)
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
     driver.get("https://www.whatismybrowser.com/")
     driver.maximize_window()
     time.sleep(5)
