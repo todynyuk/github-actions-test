7cd497b62afd430cc273084765a05dc3fd44adf0
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-09 17:02:50 +0300

    Update TestOptions.py

--- src/TestCases/TestOptions.py
+++ src/TestCases/TestOptions.py
@@ -9,17 +9,21 @@ def test_options():
     #'%(asctime)s - %(levelname)s - %(message)s'
     #logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.DEBUG)
     logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+    logger = logging.getLogger('example_logger')
+    logger.warning('This is a warning')
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    logging.info("browser opened")
-    logging.info('Opening the page...')
+    #logging.info("browser opened")
+    #logging.info('Opening the page...')
     print("Test Print Function In Logs Artifact")
     driver.get("https://www.whatismybrowser.com/")
     driver.maximize_window()
     time.sleep(5)
+    print("================Check loggin start======================")
     logging.info(os.getcwd())
+    print("================Check loggin finish======================")
     if os.path.exists(os.path.join(os.getcwd(), 'output')):
         shutil.rmtree(os.path.join(os.getcwd(), 'output'))
 
