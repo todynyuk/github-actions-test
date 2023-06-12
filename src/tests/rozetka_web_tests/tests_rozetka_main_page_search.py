43cd4bf70f56edad919b90238a625fb0bffcc97c
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-12 18:44:15 +0300

    Update tests_rozetka_main_page_search.py

--- src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
+++ src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
@@ -1,28 +1,30 @@
-import time
-from pages.main_page import MainPage
 import pytest
-import logging
+from selenium.webdriver.common.by import By
+from loguru import logger
+import pyautogui
+import time,os
 
 class TestsRozetkaMainPageSearch:
 
     @pytest.mark.label("Search", "correct")
     def test_correct_search(self, driver):
-        main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
-        main_page.open()
         search_text = "Agm A9"
-        main_page.set_search_input(search_text)
-        main_page.click_search_button()
-        logging.INFO('Test was successful')
-        assert main_page.verify_is_search_brand_present_in_goods_title(search_text), "Search text not" \
-                                                                                     " contains in all " \
-                                                                                     "goods title texts"
-
-    @pytest.mark.label("Search", "Incorrect")
-    def test_incorrect_search(self, driver):
+        myScreenshot = pyautogui.screenshot()
+        logger.remove(0)
+        logger.add(sys.stdout, level="TRACE") 
         main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
         main_page.open()
-        search_text = "hgvhvg"
-        main_page.set_search_input(search_text)
-        main_page.click_search_button()
-        logging.INFO('Test was successful')
-        assert main_page.verify_wrong_search_request(), "Wrong request text isn`t presented"
+        time.sleep(2)
+        myScreenshot.save(r'output/screen_main_page.png')
+        logger.info("Rozetka main page is opened")
+        driver.maximize_window()
+        driver.find_element(By.CSS_SELECTOR, "input[name='search']").send_keys(search_text)
+        driver.find_element(By.CSS_SELECTOR, "button[class*='button_color_green']").click()
+        button = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='goods-tile__title']"))).text
+        myScreenshot.save(r'output/screen_search_result.png')
+        assert search_text.lower() in str(button).lower(), "Search text not contains in all goods title texts"
+        logger.info('Test was successful')
+        if os.path.exists(os.path.join(os.getcwd(), 'output')):
+            shutil.rmtree(os.path.join(os.getcwd(), 'output'))
+
+        os.mkdir('output')
