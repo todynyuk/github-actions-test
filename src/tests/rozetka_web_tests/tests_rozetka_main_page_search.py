6470e39f6e9e80730e8e731f80f4b54d5a0c241d
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-12 19:46:56 +0300

    Update tests_rozetka_main_page_search.py

--- src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
+++ src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
@@ -7,24 +7,22 @@ class TestsRozetkaMainPageSearch:
 
     @pytest.mark.label("Search", "correct")
     def test_correct_search(self, driver):
-        if os.path.exists(os.path.join(os.getcwd(), 'output')):
-            shutil.rmtree(os.path.join(os.getcwd(), 'output'))
-
-        os.mkdir('output')
         search_text = "Agm A9"
         logger.remove(0)
         logger.add(sys.stdout, level="TRACE") 
         main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
         main_page.open()
         time.sleep(2)
-        driver.save_screenshot('output/screen_main_page.png')
         logger.info("Rozetka main page is opened")
         driver.maximize_window()
         driver.find_element(By.CSS_SELECTOR, "input[name='search']").send_keys(search_text)
         driver.find_element(By.CSS_SELECTOR, "button[class*='button_color_green']").click()
         button = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='goods-tile__title']"))).text
-        driver.save_screenshot('output/screen_search_result.png')
         assert search_text.lower() in str(button).lower(), "Search text not contains in all goods title texts"
         logger.info('Test was successful')
         time.sleep(5)
+        if os.path.exists(os.path.join(os.getcwd(), 'output')):
+            shutil.rmtree(os.path.join(os.getcwd(), 'output'))
+        os.mkdir('output')
+        driver.save_screenshot('output/screen.png')
         driver.close()
