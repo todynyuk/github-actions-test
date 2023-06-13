f50e35b89e668fb7276b41bfa5679a9281e5bb02
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-13 10:16:13 +0300

    Update tests_rozetka_main_page_search.py

--- src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
+++ src/tests/rozetka_web_tests/tests_rozetka_main_page_search.py
@@ -1,6 +1,5 @@
 import pytest
 from selenium.webdriver.common.by import By
-from loguru import logger
 import time,os,shutil
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -16,26 +15,16 @@ class TestsRozetkaMainPageSearch:
         options = Options()
         options.add_argument('--headless')
         options.add_argument('--disable-gpu')
-        logger = logging.getLogger('test_application')
-        logger.setLevel(logging.DEBUG)
         driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
         search_text = "Agm A9"
-        logger.remove(0)
-        logger.add(sys.stdout, level="TRACE") 
         main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
         main_page.open()
         time.sleep(2)
-        logger.info("Rozetka main page is opened")
         driver.maximize_window()
         driver.find_element(By.CSS_SELECTOR, "input[name='search']").send_keys(search_text)
         driver.find_element(By.CSS_SELECTOR, "button[class*='button_color_green']").click()
         button = WebDriverWait(driver, 3).until(EC.visibility_of_element_located((By.XPATH, "//span[@class='goods-tile__title']"))).text
         assert search_text.lower() in str(button).lower(), "Search text not contains in all goods title texts"
-        logger.info('Test was successful')
-        fh = logging.FileHandler('report.log')
-        fh.setLevel(logging.DEBUG)
-        logging.info("This is standard logging before test")
-        logger.addHandler(fh)
         logging.info("This is standard logging after test")
         print("This is standard print test")
         time.sleep(5)
