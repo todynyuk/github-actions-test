0ddda04d4677b9807bf90d157fa4e090acc102fb
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-14 19:24:45 +0300

    Update test_PDP.py

--- src/rozetka_web_tests/test_PDP.py
+++ src/rozetka_web_tests/test_PDP.py
@@ -2,6 +2,7 @@ import pytest
 import time,os,re,shutil
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,7 +12,10 @@ from selenium.webdriver import ActionChains
 def setup():
     test_url = "https://rozetka.com.ua/ua/"
     global driver
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     driver.maximize_window()
     driver.get(test_url)
     driver.implicitly_wait(10)
