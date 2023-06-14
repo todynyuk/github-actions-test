ce4d0a2b169cc3d1f87fff6335a5e5acc3eb7482
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-14 12:14:27 +0300

    Update test_PDP.py

--- src/tests/rozetka_web_tests/test_PDP.py
+++ src/tests/rozetka_web_tests/test_PDP.py
@@ -3,35 +3,27 @@ from pages.device_page import DevicePage
 from pages.devices_category_page import DeviceCategory
 from pages.main_page import MainPage
 from pages.sub_category_page import SubCategory
-from loguru import logger
 import time,os,sys
 
 class TestDetailsPage:
     def testItemRamAndPrice(self):
-        logger.remove(0)
-        logger.add(sys.stdout, level="TRACE") 
-        main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
+        main_page = MainPage('https://rozetka.com.ua/ua/')
         main_page.open()
         time.sleep(2)
         print("This is standard print test")
-        logger.info("Rozetka main page is opened")
-        main_page.click_universal_category_link(driver, "Смартфони")
+        main_page.click_universal_category_link("Смартфони")
         time.sleep(2)
-        logger.info("Page Smartphones is opened")
-        SubCategory.click_universal_subcategory_menu_link(self, "Мобільні", driver)
+        SubCategory.click_universal_subcategory_menu_link(self, "Мобільні")
         time.sleep(2)
-        logger.info("Page Mobile is opened")
-        DeviceCategory.choose_ram_сapacity(self, driver, 12)
-        DeviceCategory.click_check_box_filter(self, driver, "Синій")
-        smartphone_price = DeviceCategory.getSmartphonePriceText(self, driver, 1)
-        DeviceCategory.clickLinkMoreAboutDevice(self, driver, 1)
+        DeviceCategory.choose_ram_сapacity(self,12)
+        DeviceCategory.click_check_box_filter(self,"Синій")
+        smartphone_price = DeviceCategory.getSmartphonePriceText(self,1)
+        DeviceCategory.clickLinkMoreAboutDevice(self,1)
         time.sleep(2)
-        logger.info("Page MoreAboutDevice is opened")
-        short_characteristics = DevicePage.verify_device_short_characteristic(self, driver, 12)
-        chosen_device_price = DevicePage.get_chosen_product_price(self, driver)
+        short_characteristics = DevicePage.verify_device_short_characteristic(self,12)
+        chosen_device_price = DevicePage.get_chosen_product_price(self,)
         assert short_characteristics, "Short_characteristics don't contains chosen ram capacity"
         assert str(smartphone_price) == chosen_device_price, "Prices are not equals"
-        logger.info("Test was successful")
         if os.path.exists(os.path.join(os.getcwd(), 'output')):
             shutil.rmtree(os.path.join(os.getcwd(), 'output'))
         os.mkdir('output')
