901e37c6db786b8f97f5f395b3bee5fa6705829a
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-14 12:34:16 +0300

    Update test_PDP.py

--- src/tests/rozetka_web_tests/test_PDP.py
+++ src/tests/rozetka_web_tests/test_PDP.py
@@ -6,25 +6,24 @@ from pages.sub_category_page import SubCategory
 import time,os,sys
 
 class TestDetailsPage:
-    def testItemRamAndPrice(self):
-        main_page = MainPage('https://rozetka.com.ua/ua/')
+    def testItemRamAndPrice(self,driver):
+        main_page = MainPage(driver, 'https://rozetka.com.ua/ua/')
         main_page.open()
         time.sleep(2)
         print("This is standard print test")
-        main_page.click_universal_category_link("Смартфони")
+        main_page.click_universal_category_link(driver, "Смартфони")
         time.sleep(2)
-        SubCategory.click_universal_subcategory_menu_link(self, "Мобільні")
+        SubCategory.click_universal_subcategory_menu_link(self, "Мобільні", driver)
         time.sleep(2)
-        DeviceCategory.choose_ram_сapacity(self,12)
-        DeviceCategory.click_check_box_filter(self,"Синій")
-        smartphone_price = DeviceCategory.getSmartphonePriceText(self,1)
-        DeviceCategory.clickLinkMoreAboutDevice(self,1)
+        DeviceCategory.choose_ram_сapacity(self, driver, 12)
+        DeviceCategory.click_check_box_filter(self, driver, "Синій")
+        smartphone_price = DeviceCategory.getSmartphonePriceText(self, driver, 1)
+        DeviceCategory.clickLinkMoreAboutDevice(self, driver, 1)
         time.sleep(2)
-        short_characteristics = DevicePage.verify_device_short_characteristic(self,12)
-        chosen_device_price = DevicePage.get_chosen_product_price(self,)
+        short_characteristics = DevicePage.verify_device_short_characteristic(self, driver, 12)
+        chosen_device_price = DevicePage.get_chosen_product_price(self, driver)
         assert short_characteristics, "Short_characteristics don't contains chosen ram capacity"
         assert str(smartphone_price) == chosen_device_price, "Prices are not equals"
         if os.path.exists(os.path.join(os.getcwd(), 'output')):
             shutil.rmtree(os.path.join(os.getcwd(), 'output'))
         os.mkdir('output')
-        time.sleep(2)
