912730fc4191c2b038a32260a384527347bcf7b8
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-14 19:47:18 +0300

    Update test_PDP.py

--- src/rozetka_web_tests/test_PDP.py
+++ src/rozetka_web_tests/test_PDP.py
@@ -17,19 +17,17 @@ def setup():
     options.add_argument('--headless')
     options.add_argument('--disable-gpu')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver.maximize_window()
-    driver.get(test_url)
-    driver.implicitly_wait(10)
-    yield
-    driver.quit()
 
 @pytest.mark.maintainer("todynyuk")
 @pytest.mark.label("ItemFilter", "RamAndPrice")
 def testItemRamAndPrice(setup):
         driver.get("https://rozetka.com.ua/ua/")
-        time.sleep(2)
+        driver.maximize_window()
+        driver.implicitly_wait(10)
         print("This is standard print test")
-        driver.find_element(By.XPATH, "//a[@class='menu-categories__link' and contains(.,'Смартфони')]").click()
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//a[@class='menu-categories__link' and contains(.,'Смартфони')]"))).click()  
         time.sleep(2)
         print("Page Smartphones is opened")
         driver.find_element(By.XPATH,
