6a9fbd9d8b1521469c3ef2ed97e676d3a69dae0b
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-16 12:38:03 +0300

    Update test_PDP.py

--- src/rozetka_web_tests/test_PDP.py
+++ src/rozetka_web_tests/test_PDP.py
@@ -31,6 +31,31 @@ def testItemRamAndPrice(setup):
         logging.warning(str(driver.title))
         verify_title = str(driver.title).lower().__contains__("rozetka")
         assert verify_title, " Title not contains |rozetka| "
+        driver.find_element(By.XPATH, "//a[@class='menu-categories__link' and contains(.,'Смартфони')]").click() 
+        time.sleep(2)
+        print("Page Smartphones is opened")
+        driver.find_element(By.XPATH,"//a[contains(@class,'tile-cats__heading') and contains(.,'Мобільні')]").click()
+        time.sleep(2)
+        print("Mobile Page Smartphones is opened")
+        WebDriverWait(driver, 10).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//a[contains(@class,'tile-filter__link') and contains(text(),'12')]"))).click()
+        time.sleep(2)
+        element = driver.find_element(By.XPATH, "//a[contains(@data-id,'Синій')]")
+        action = ActionChains(driver)
+        action.move_to_element(element).perform()
+        element.click()
+        time.sleep(2)
+        driver.execute_script("window.scrollTo(0, 220)")
+        smartphone_price = int(re.sub(r'\D', '', driver.find_element(By.XPATH, "//span[@class='goods-tile__price-value'][1]").text))
+        driver.execute_script("window.scrollTo(0, 220)")
+        driver.find_element(By.XPATH, "//a[@class='goods-tile__heading ng-star-inserted'][1]").click()
+        time.sleep(2)
+        print("MoreAboutDevice Page Smartphones is opened")
+        short_characteristics = driver.find_element(By.XPATH, "//h1[@class='product__title']").text.__contains__(str(12))
+        chosen_device_price = re.sub(r'\D', '', driver.find_element(By.XPATH, "//p[contains(@class,'product-price__big')]").text)
+        assert short_characteristics, "Short_characteristics don't contains chosen ram capacity"
+        assert str(smartphone_price) == chosen_device_price, "Prices are not equals"
         print("Test was successful")
         driver.save_screenshot('output/screen.png')
         driver.quit()
