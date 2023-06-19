2649295620c02fbf7a307fc714e7a91286b2f15b
todynyuk <110404091+todynyuk@users.noreply.github.com>
2023-06-19 12:44:40 +0300

    Update and rename tests_rozetka_main_page_search_py to tests_rozetka_main_page_search.py

---
+++ src/rozetka_web_tests/tests_rozetka_main_page_search.py
@@ -0,0 +1,69 @@
+import pytest
+from selenium.webdriver.common.by import By
+import time,os,shutil
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import sys
+import logging
+from loguru import logger
+import undetected_chromedriver as uc
+
+def setup():
+    test_url = "https://rozetka.com.ua/ua/"
+    global driver
+    driver = uc.Chrome(headless=True,use_subprocess=False)
+    driver.get(test_url)
+    time.sleep(2)
+    driver.maximize_window()
+    driver.implicitly_wait(10)
+    yield
+    driver.quit()
+
+def test_correct_search():
+    logger.remove(0)
+    logger.add(sys.stdout, level="TRACE") 
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.setLevel(logging.DEBUG) 
+    stdout_handler = logging.StreamHandler(stream=sys.stdout)
+    format_output = logging.Formatter('%(levelname)s : %(name)s : %(message)s : %(asctime)s') # <-
+    stdout_handler.setFormatter(format_output) 
+    LOGGER.addHandler(stdout_handler)
+    
+    assert str(driver.title).lower().__contains__("rozetka"), " Title not contains |rozetka| "
+    logger.info("This is an info message.")
+    search_text = "Agm A9"
+    driver.find_element(By.CSS_SELECTOR, "input[name='search']").send_keys(search_text)
+    driver.find_element(By.CSS_SELECTOR, "button[class*='button_color_green']").click()
+    counter = 0
+    WebDriverWait(driver, 3).until(EC.visibility_of_all_elements_located((By.XPATH, "//span[@class='goods-tile__title']")))    
+    goods_title = driver.find_elements(By.XPATH, "//span[@class='goods-tile__title']")
+    for x in range(1, 6):
+        if goods_title[x].text.lower().__contains__(search_text.lower()):
+            counter += 1
+        else:
+            counter += 0 
+    assert counter == 5, "Search text not contains in all goods title texts"
+    LOGGER.info("This is standard logging after test")
+    print("This is standard print test")
+    time.sleep(5)
+    driver.get_screenshot_as_file('screen.png')
+    driver.save_screenshot('output/screen.png')
+    if os.path.exists(os.path.join(os.getcwd(), 'output')):
+        shutil.rmtree(os.path.join(os.getcwd(), 'output'))
+    os.mkdir('output')
+    driver.get_screenshot_as_file('output/screen.png')
+    driver.save_screenshot('output/screen.png')
+    driver.get_screenshot_as_file('screen.png')
+    time.sleep(2)
+    driver.close()
+
+    def test_incorrect_search():
+    assert str(driver.title).lower().__contains__("rozetka"), " Title not contains |rozetka| "
+    logger.info("This is an info message.")
+    search_text = "hgvhvg"
+    driver.find_element(By.CSS_SELECTOR, "input[name='search']").send_keys(search_text)
+    driver.find_element(By.CSS_SELECTOR, "button[class*='button_color_green']").click()
+    assert driver.find_element(By.XPATH, "//span[@class='ng-star-inserted']").is_displayed(), "Wrong request text isn`t presented"
+    logging.INFO('Test was successful')
